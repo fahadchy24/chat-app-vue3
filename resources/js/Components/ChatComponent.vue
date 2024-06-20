11688b38cbe3bcee34f0d12a93eca70ce572e87a
fahadchy24 <fahadchy24@gmail.com>
2024-06-20 17:16:54 +0600

    Added: user typing notification

--- resources/js/Components/ChatComponent.vue
+++ resources/js/Components/ChatComponent.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from "axios";
-import { nextTick, onMounted, ref, watch } from "vue";
+import {nextTick, onMounted, ref, watch} from "vue";
 
 const props = defineProps({
     friend: {
@@ -16,8 +16,10 @@ const props = defineProps({
 const messages = ref([]);
 const newMessage = ref("");
 const messagesContainer = ref("");
+const isFriendTyping = ref(false);
+const isFriendTypingTimer = ref(null);
 
-watch(messages, () =>{
+watch(messages, () => {
     nextTick(() => {
         // messagesContainer.value.scrollTop = messagesContainer.value.scrollHeight;
         messagesContainer.value.scrollTo({
@@ -40,6 +42,13 @@ const sendMessage = () => {
     }
 };
 
+const sendTypingEvent = () => {
+    Echo.private(`chat.${props.friend.id}`)
+        .whisper("typing", {
+            userID: props.currentUser.id,
+        });
+};
+
 onMounted(() => {
     axios.get(`/messages/${props.friend.id}`).then((response) => {
         messages.value = response.data;
@@ -48,6 +57,20 @@ onMounted(() => {
     Echo.private(`chat.${props.currentUser.id}`)
         .listen('ChatMessageEvent', (response) => {
             messages.value.push(response.message);
+        })
+        .listenForWhisper("typing", (response) => {
+            /*if(response.userID === props.friend.id){
+                isFriendTyping.value = true;
+            }*/
+            isFriendTyping.value = response.userID === props.friend.id;
+
+            if (isFriendTypingTimer.value) {
+                clearTimeout(isFriendTypingTimer.value);
+            }
+
+            isFriendTypingTimer.value = setTimeout(() => {
+                isFriendTyping.value = false;
+            }, 1000);
         });
 });
 
@@ -78,6 +101,7 @@ onMounted(() => {
             <input
                 type="text"
                 v-model="newMessage"
+                @keydown="sendTypingEvent"
                 @keyup.enter="sendMessage"
                 placeholder="Type a message..."
                 class="flex-1 px-2 py-1 border rounded-lg"
@@ -89,5 +113,8 @@ onMounted(() => {
                 Send
             </button>
         </div>
+        <small v-if="isFriendTyping" class="text-gray-500">
+            {{ friend.name }} is typing...
+        </small>
     </div>
 </template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,19 +6,6 @@ const props = defineProps({
     users: Object,
 });
 
-
-function OnMounted() {
-    axios
-        .get('/users')
-        .then(response => {
-            console.log(response.data);
-            props.users.value = response.data;
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
 </script>
 
 <template>
@@ -26,7 +13,7 @@ function OnMounted() {
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboardsss</h2>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h2>
         </template>
 
         <div class="py-12">
@@ -34,12 +21,10 @@ function OnMounted() {
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                            <!-- Loop through the users array to display user information -->
                             <template v-for="user in users" :key="user.id">
                                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                                     <div class="p-6">
                                         <div class="flex items-center">
-                                            <!-- Create a link to the chat route for each user -->
                                             <Link :href="route('chat', user.id)">
                                                 <span class="text-lg font-medium">{{ user.name }}</span>
                                             </Link>
